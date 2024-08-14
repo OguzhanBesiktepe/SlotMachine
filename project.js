2144a16b619e2414886aad128dc456b6ff148ba1
OguzhanBesiktepe <besio@farmingdale.edu>
2024-08-14 12:36:03 -0400

    Step 2 Code is Structured Very Similarly to step 1. we added new constant variables and functionswith related if statements with OR properties.

--- project.js
+++ project.js
@@ -34,6 +34,22 @@ const deposit = () => {
     }
 };
 
-//calling the function
+// Step 2: Determine Number of Lines to Bet On.
+
+//Below Code is Very Similar to its structure in Step 1.
+
+const getNumberOfLines = () => {
+    while (true){
+        const lines = prompt("Enter the number of lines to bet on (1-3): ")
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.")
+            } else {
+                return numberOfLines;
+            }
+        }
+};
+
 const depositAmount = deposit();
-console.log(depositAmount);
+const numberOfLines = getNumberOfLines();
